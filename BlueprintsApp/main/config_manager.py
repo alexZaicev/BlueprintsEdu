7a0e11176d325f54b394dd12edf0d8b2d2048e52
Aleksej Zaicev <alex.zaicef@gmail.com>
2018-10-28 14:58:19 +0000

    Added configuration file generator

--- BlueprintsApp/main/config_manager.py
+++ BlueprintsApp/main/config_manager.py
@@ -31,30 +31,35 @@ class ConfigManager(object):
     def set_configurations(cls):
         cfgs = dict()
         if not os.path.exists(ConfigManager.CONFIG_PATH):
-            ConfigManager.LOGGER.error("Configuration file not found... Make sure file is located in the root path")
-            raise ImportError("Configuration file not found... Make sure file is located in the root path")
-        else:
-
-            with open(ConfigManager.CONFIG_PATH, 'r') as json_cfg:
-                cfg = json.load(json_cfg)
-                # LOADING CONFIGURATIONS
-                # TRY to find customed settings
-                try:
-                    ConfigManager.LOGGER.debug("Loading custom configuration settings...")
-                    cfgs["screen"] = [cfg["CUSTOM"]["SIZE"]["WIDTH"], cfg["CUSTOM"]["SIZE"]["HEIGHT"]]
-                    cfgs["lang"] = cfg["CUSTOM"]["LANGUAGE"]
-                    cfgs["theme"] = cfg["CUSTOM"]["THEME"]
-
-                except KeyError as ex:
-                    ConfigManager.LOGGER.error("Custom configurations not found")
-                    ConfigManager.LOGGER.debug("Loading default configurations...")
-                    cfgs["screen"] = [cfg["DEFAULT"]["SIZE"]["WIDTH"], cfg["DEFAULT"]["SIZE"]["HEIGHT"]]
-                    cfgs["lang"] = cfg["DEFAULT"]["LANGUAGE"]
-                    cfgs["theme"] = cfg["DEFAULT"]["THEME"]
+            ConfigManager.LOGGER.error("Configuration file not found...")
+            ConfigManager.LOGGER.info("Generating default configuration file...")
+            ConfigManager.generate_default_configuration()
+        with open(ConfigManager.CONFIG_PATH, 'r') as json_cfg:
+            cfg = json.load(json_cfg)
+            # LOADING CONFIGURATIONS
+            # TRY to find customed settings
+            try:
+                ConfigManager.LOGGER.info("Loading custom configuration settings...")
+                cfgs["screen"] = [cfg["CUSTOM"]["SIZE"]["WIDTH"], cfg["CUSTOM"]["SIZE"]["HEIGHT"]]
+                cfgs["lang"] = cfg["CUSTOM"]["LANGUAGE"]
+                cfgs["theme"] = cfg["CUSTOM"]["THEME"]
+            except KeyError as ex:
+                ConfigManager.LOGGER.error("Custom configurations not found")
+                ConfigManager.LOGGER.info("Loading default configurations...")
+                cfgs["screen"] = [cfg["DEFAULT"]["SIZE"]["WIDTH"], cfg["DEFAULT"]["SIZE"]["HEIGHT"]]
+                cfgs["lang"] = cfg["DEFAULT"]["LANGUAGE"]
+                cfgs["theme"] = cfg["DEFAULT"]["THEME"]
         app_utils.BOARD_WIDTH = cfgs.get("screen")[0]  # SCREEN SETTINGS
         app_utils.BOARD_HEGHT = cfgs.get("screen")[1]
         StringUtils.set_language(cfgs.get("lang"))  # LANGUAGE
         Themes.set_theme(cfgs.get("theme"))  # THEME
+
+    @classmethod
+    def generate_default_configuration(cls):
+        cfg_dict = dict()
+        cfg_dict["DEFAULT"] = ConfigManager.DEFAULT_CONFIG
+        with open(ConfigManager.CONFIG_PATH, 'w+') as cfg_file:
+            json.dump(cfg_dict, cfg_file)
 
     @classmethod
     def save_configurations(cls):
